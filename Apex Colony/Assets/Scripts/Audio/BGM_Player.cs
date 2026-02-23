540d8a3a2ad142e4758f8364d68848c7dda5bc85
Huntrt <58127655+Huntrt@users.noreply.github.com>
2026-02-23 20:14:13 +0700

    Automatic populate level music track to be playable

--- Apex Colony/Assets/Scripts/Audio/BGM_Player.cs
+++ Apex Colony/Assets/Scripts/Audio/BGM_Player.cs
@@ -1,13 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BGM_Player : MonoBehaviour
 {
 	public int defaultTransition = -1;
-	[SerializeField] Transition[] transitions;
+	[SerializeField] List<Transition> transitions = new();
+	[SerializeField] AudioClip[] levelsBGM;
+	[SerializeField] float levelBGMTransitionSpeed;
+	bool populatedTransitionsWithLevelBGM;
 
 	[System.Serializable] public class Transition
 	{
-		[SerializeField] AudioClip clip; public AudioClip Clip {get => clip;}
+		[SerializeField] AudioClip clip; public AudioClip Clip {get => clip; set => clip = value;}
 		public float exitDuration, enterDuration;
 		[Tooltip("When this exit it will continue whatever enter transition left off")]
 		[SerializeField] bool continueWhenExit; public bool ContinueWhenExit {get => continueWhenExit;} 
@@ -28,5 +32,23 @@ public class BGM_Player : MonoBehaviour
 			return;
 		}
 		Debug.LogWarning("There are no transition data for '" + audioClip.name + "' audio clip");
+	}
+
+	public void PlayLevelBGM()
+	{
+		//Auto populate transition data with level bgm since it generic
+		if(!populatedTransitionsWithLevelBGM)
+		{
+			foreach (AudioClip bgm in levelsBGM)
+			{
+				Transition transition = new();
+				transition.Clip = bgm;
+				transition.exitDuration = levelBGMTransitionSpeed;
+				transition.enterDuration = levelBGMTransitionSpeed;
+				transitions.Add(transition);
+			}
+
+			populatedTransitionsWithLevelBGM = true;
+		}
 	}
 }
